0b094ee886a9138244ad93857f4ff3790768430b
jonathanwoolf <jwool003@ucr.edu>
2018-10-10 15:04:05 -0700

    part 2 updated

--- lab2/jwool003_lab2_part2/jwool003_lab2_part2/main.c
+++ lab2/jwool003_lab2_part2/jwool003_lab2_part2/main.c
@@ -24,75 +24,72 @@ int uart_tick(int state){
 	tmpA = ~PINA & 0x01;
 	switch(state){
 		case uart_start:
-		state = send;
-		break;
-		case check:
-		state = send;
-		break;
-		case send:
-		state = toggle;
-		break;
-		case toggle:
-		state = check;
-		break;
-		
-		default:
-		state = uart_start;
-		break;
-	}
-	switch(state){
-		case uart_start:
-		tmpB = 0x00;
-		tmpC = 0x00;
-		leader = 0;
-		s_data = 0x00;
-		break;
-		case check:
-		if(tmpA == 0x01){
-			leader = 1;
-			tmpC = 0x01;
-			tasks[0].period = 1000;
-		}
-		else{
-			leader = 0;
+			tmpB = 0x00;
 			tmpC = 0x00;
-			tasks[0].period = 10;
-		}
-		break;
+			leader = 0;
+			s_data = 0x00;
+			state = send;
+			break;
+		case check:
+			if(tmpA == 0x01)
+			{
+				leader = 1;
+				tmpC = 0x01;
+				tasks[0].period = 1000;
+			}
+			else
+			{
+				leader = 0;
+				tmpC = 0x00;
+				tasks[0].period = 10;
+			}
+			state = send;
+			break;
 		case send:
-		if(leader == 1){
-			if(USART_IsSendReady(1)){
-				state = send;
-				USART_Send(s_data, 1);
-				tmpB = s_data;
+			if(leader == 1)
+			{
+				if(USART_IsSendReady(1))
+				{
+					state = send;
+					USART_Send(s_data, 1);
+					tmpB = s_data;
+				}
 			}
-		}
-		else{
-			if(USART_HasReceived(0)){
-				r_data = USART_Receive(0);
+			else
+			{
+				if(USART_HasReceived(0))
+				{
+					r_data = USART_Receive(0);
+				}
 			}
-		}
-		break;
+			state = toggle;
+			break;
 		case toggle:
-		if(leader == 1){
-			if(USART_HasTransmitted(1)){
-				if(s_data == 0x00){
-					s_data = 0x01;
-				}
-				else{
-					s_data = 0x00;
+			if(leader == 1)
+			{
+				if(USART_HasTransmitted(1))
+				{
+					if(s_data == 0x00)
+					{
+						s_data = 0x01;
+					}
+					else
+					{
+						s_data = 0x00;
+					}
 				}
 			}
-		}
-		else{
-			tmpB = r_data;
-		}
-		break;
-		
+			else
+			{
+				tmpB = r_data;
+			}
+			state = check;
+			break;
 		default:
-		tmpB = 0x00;
-		tmpC = 0x00;
-		break;
+			tmpB = 0x00;
+			tmpC = 0x00;
+			state = uart_start;
+			break;
 	}
 	PORTB = tmpB;
 	PORTC = tmpC;
@@ -101,23 +98,22 @@ int uart_tick(int state){
 
 int main(void)
 {
-	unsigned char i = 0;
-	tasks[i].state = uart_start;
-	tasks[i].period = 1000;
-	tasks[i].elapsedTime = 0;
-	tasks[i].TickFct = &uart_tick;
-
-	/* Replace with your application code */
-	
-	DDRA = 0x00; PORTA = 0xFF;
+	DDRA = 0x00; PORTA = 0xFF; // PINA set to input
 	DDRB = 0xFF; PORTB = 0x00; // PORTB set to output, outputs init 0s
 	DDRC = 0xFF; PORTC = 0x00;
+		
 	
 	TimerSet(timerPeriod);
 	TimerOn();
 	
 	initUSART(0);
 	initUSART(1);
+	
+	unsigned char i = 0;
+	tasks[i].state = uart_start;
+	tasks[i].period = 1000;
+	tasks[i].elapsedTime = 0;
+	tasks[i].TickFct = &uart_tick;
 	
 	while(1)
 	{
