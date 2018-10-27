467d2cc981835330b5d3ec007b0e4bdda8ba4754
jonathan <jwool003@ucr.edu>
2018-10-27 02:08:59 -0700

    Update

--- lab6/jwool003_lab6_part1/jwool003_lab6_part1/main.c
+++ lab6/jwool003_lab6_part1/jwool003_lab6_part1/main.c
@@ -4,13 +4,26 @@
  * Created: 10/25/2018 3:55:42 PM
  * Author : Jonathan
  */ 
+#define timerPeriod 1
+#define tasksNum 1
 
 #include <avr/io.h>
+#include "scheduler.h"
+
+void A2D_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: Enables analog-to-digital conversion
+	// ADSC: Starts analog-to-digital conversion
+	// ADATE: Enables auto-triggering, allowing for constant
+	//	    analog to digital conversions.
+}
 
 
 int main(void)
 {
-    /* Replace with your application code */
+	
+    A2D_init();
+	
     while (1) 
     {
     }
