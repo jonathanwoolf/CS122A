09e1425810923147714e04767f933e7004ababad
jonathanwoolf <jwool003@ucr.edu>
2018-10-30 18:05:43 -0700

    SAVVY START

---
+++ S.A.V.V.Y/S.A.V.V.Y/main.c
@@ -0,0 +1,18 @@
+/*
+ * S.A.V.V.Y.c
+ *
+ * Created: 10/30/2018 5:56:21 PM
+ * Author : Jonathan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
